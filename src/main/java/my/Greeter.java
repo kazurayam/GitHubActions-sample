9ea42c4c4913d6b856f0522c6fa8e21d838ec600
kazurayam <kazuaki.urayama@gmail.com>
2022-11-21 21:02:04 +0900

    created GitHub Actions junit.yml

---
+++ src/main/java/my/Greeter.java
@@ -0,0 +1,11 @@
+package my;
+
+public class Greeter {
+
+    public Greeter() {}
+
+    public String greet(String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+}
---
+++ src/test/java/my/GreeterTest.java
@@ -0,0 +1,15 @@
+package my;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreeterTest {
+
+    @Test
+    public void test_greet() {
+        String expected = "Hello, world!";
+        String actual = new Greeter().greet("world");
+        assertEquals(expected, actual);
+    }
+}
